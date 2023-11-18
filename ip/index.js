5c4e5c4579631f652cf216b05351392e572ab52b
dogan-keles <131661707+dogan-keles@users.noreply.github.com>
2023-11-18 16:38:08 +0300

    ip-geolocation-api-done

--- ip/index.js
+++ ip/index.js
@@ -1,4 +1,3 @@
-//axios import buraya gelecek
 import axios from "axios";
 
 var benimIP;
@@ -24,17 +23,16 @@ async function ipAdresimiAl() {
 /*
 	ADIM 1: axios kullanarak, aşağıdaki URL'ye GET sorgusu atacağız
     (tag içindeki yere kendi ipnizi yazarak URL'yi oluşturun):
-    https://apis.ergineer.com/ipgeoapi/<ipniz>
+    https://apis.ergineer.com/ipgeoapi/94.55.132.38
 	
 	NOT: Bilgisayarın IP adresini öğrenmek için: https://apis.ergineer.com/ipadresim 
 	ADIM 5'e gelene kadar fonksiyonunuzu test etmek için ip nizi URL'ye manuel olarak ekleyebilirsiniz.
 */
-const myIp = "88.229.104.38";
+
 /*
 	ADIM 2: Geri döndürülen verileri inceleyin, bu sizin ip bilgileriniz! Bileşen fonksiyonunuzu geliştirmek içindeki bu veri yapısını
 	iyice anlamanız gerekmektedir.
 	
-	// Postmann 'den alınan "response" incelendi.
 */
 /*
 	ADIM 3: Argümanı sadece 1 nesne kabül eden bir fonksiyon oluşturun.
@@ -54,10 +52,55 @@ const myIp = "88.229.104.38";
     </div>
 */
 
+const nesneliFonksiyon = (nesne) => {
+  const cardDiv = document.createElement("div");
+  cardDiv.setAttribute("class", "card");
+  const flagImg = document.createElement("img");
+  flagImg.setAttribute("src", "https://flagsapi.com/TR/shiny/64.png");
+  const cardInfoDiv = document.createElement("div");
+  cardInfoDiv.setAttribute("class", "card-info");
+  const h3 = document.createElement("h3");
+  h3.setAttribute("class", "ip");
+  h3.textContent = nesne["sorgu"];
+  const pUlke = document.createElement("p");
+  pUlke.setAttribute("class", "ulke");
+  pUlke.textContent = nesne["ülke"] + "(" + nesne["ülkeKodu"] + ")";
+  const pEnlem = document.createElement("p");
+  pEnlem.textContent =
+    "Enlem: " + nesne["enlem"] + " Boylam: " + nesne["boylam"];
+  const pSehir = document.createElement("p");
+  pSehir.textContent = "Sehir: " + nesne["şehir"];
+  const pSaat = document.createElement("p");
+  pSaat.textContent = "Saat Dilimi: " + nesne["saatdilimi"];
+  const pPara = document.createElement("p");
+  pPara.textContent = "Para Birimi: " + nesne["parabirimi"];
+  const pIsp = document.createElement("p");
+  pIsp.textContent = "ISP : " + nesne["isp"];
+
+  cardInfoDiv.append(h3, pUlke, pEnlem, pSehir, pSaat, pPara, pIsp);
+  cardDiv.append(flagImg);
+  cardDiv.append(cardInfoDiv);
+
+  console.log("Card oluşturuldu. 4");
+
+  return cardDiv;
+};
+
 /*
 	ADIM 4: API'den alınan verileri kullanarak ADIM 3'te verilen yapıda bir kart oluşturun ve 
 	bu kartı DOM olarak .cards elementinin içine ekleyin. 
 */
+
+console.log("Axios Öncesi :1 ");
+axios
+  .get("https://apis.ergineer.com/ipgeoapi/85.105.61.97")
+  .then((response) => {
+    console.log("Axios Döndü : 2");
+    document.querySelector(".cards").append(nesneliFonksiyon(response.data));
+    return response.data;
+  });
+
+console.log("Axios Sonrası : 3");
 
 /*
 	ADIM 5: Manuel olarak eklediğiniz IP adresini dinamiğe dönüştürün. 
@@ -68,23 +111,3 @@ const myIp = "88.229.104.38";
 */
 
 //kodlar buraya gelecek
-const cardsContainer = document.querySelector(".cards");
-const htmlCard = `	<div class="card">
-<img src={ülke bayrağı url} />
-<div class="card-info">
-	<h3 class="ip">{ip adresi}</h3>
-	<p class="ulke">{ülke bilgisi (ülke kodu)}</p>
-	<p>Enlem: {enlem} Boylam: {boylam}</p>
-	<p>Şehir: {şehir}</p>
-	<p>Saat dilimi: {saat dilimi}</p>
-	<p>Para birimi: {para birimi}</p>
-	<p>ISP: {isp}</p>
-</div>
-</div>`;
-
-async function getLocationInfos(ipAdd) {
-  await setTimeout(() => {
-    cardsContainer.innerHTML = htmlCard;
-  }, 2000);
-}
-getLocationInfos();
